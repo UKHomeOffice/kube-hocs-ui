ddc7051df316f07abcbca036169d93b874b86b65
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2017-11-02 11:52:11 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ export WHITELIST=${WHITELIST:-0.0.0.0/0}
 if [ $ENVIRONMENT == "prod" ]
 then
     export KUBE_TOKEN=${PROD_KUBE_TOKEN}
+    export DNS_PREFIX=
+else
+    export DNS_PREFIX=${ENVIRONMENT}.notprod.
 fi
 
 cd kd
