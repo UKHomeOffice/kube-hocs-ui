18f81e0fbc727b485bd6ab2e2240a10c117a47b4
Nick McMahon <nick.mcmahon@digital.homeoffice.gov.uk>
2018-10-11 12:04:36 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,52 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-if [ $ENVIRONMENT == "prod" ]
-then
-    export KUBE_TOKEN=${PROD_KUBE_TOKEN}
-    export DNS_PREFIX=prod.
-else
-    export DNS_PREFIX=${ENVIRONMENT}.notprod.
+export KUBE_NAMESPACE=${KUBE_NAMESPACE}
+export KUBE_SERVER=${KUBE_SERVER}
+
+if [[ -z ${VERSION} ]] ; then
+    export VERSION=${IMAGE_VERSION}
 fi
 
-cd kd
-kd --insecure-skip-tls-verify --timeout 5m0s \
-   --file ingress.yaml \
-   --file service.yaml \
-   --file deployment.yaml
+if [[ ${ENVIRONMENT} == "prod" ]] ; then
+    echo "deploy ${VERSION} to prod namespace, using HOCS_UI_PROD drone secret"
+    export KUBE_TOKEN=${HOCS_UI_PROD}
+    export REPLICAS="2"
+    export DNS_PREFIX=alf
+    export CA_URL="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-prod.crt"
+else
+    export DNS_PREFIX=${ENVIRONMENT}.alf-notprod.
+    export CA_URL="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
+    if [[ ${ENVIRONMENT} == "qa" ]] ; then
+        echo "deploy ${VERSION} to test namespace, using HOCS_UI_QA drone secret"
+        export KUBE_TOKEN=${HOCS_UI_QA}
+        export REPLICAS="2"
+    else
+        echo "deploy ${VERSION} to dev namespace, HOCS_UI_QA drone secret"
+        export KUBE_TOKEN=${HOCS_UI_QA}
+        export REPLICAS="1"
+    fi
+fi
+
+export DOMAIN_NAME=${DNS_PREFIX}homeoffice.gov.uk
+
+if [[ -z ${KUBE_TOKEN} ]] ; then
+    echo "[error] Failed to find a value for KUBE_TOKEN - exiting"
+    exit -1
+elif [ ${#KUBE_TOKEN} -ne 36 ] ; then
+    echo "[error] Kubernetes token wrong length (expected 36, got ${#KUBE_TOKEN})"
+    exit 78
+fi
+
+export KUBE_CERTIFICATE_AUTHORITY=/tmp/cert.crt
+if ! wget --quiet $CA_URL -O $KUBE_CERTIFICATE_AUTHORITY; then
+    echo "[error] failed to download certificate authority!"
+    exit 1
+fi
+
+
+cd kd || exit 1
+
+kd --timeout 10m \
+   -f ingress.yaml \
+   -f service.yaml \
+   -f deployment.yaml
